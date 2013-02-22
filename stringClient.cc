48f779227b6442ba9a3f5007ee5a01b46bafaef3
Kevin C <kkevinchou@gmail.com>
2013-02-21 19:59:53 -0500

    first working client/server

--- stringClient.cc
+++ stringClient.cc
@@ -1,15 +1,72 @@
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
 
 #include "message.h"
 #include <list>
 
+
+
+
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+
 using namespace std;
-
-
-int main()
+void error(const char *msg)
 {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       cerr << "usage "<< argv[0]<<"  hostname port\n"<<endl;
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        cerr<<"ERROR, no such host"<<endl;
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+    cerr << "Please enter the message: ";
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+         error("ERROR writing to socket");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+         error("ERROR reading from socket");
+    cout << buffer << endl;
+    close(sockfd);
+
 	string msg;
 
 	list<Message* > msges;
@@ -19,7 +76,6 @@ int main()
 		Message * m = new Message(msg);
 		msges.push_front(m);
 	}
-
 
 	// eof, but wait for server to respond
 }
